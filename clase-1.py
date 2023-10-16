6cf0ce9c91b2be3ac55618ba47d979f6857bed82
pedro-damian <136197537+pedro-damian@users.noreply.github.com>
2023-10-16 17:06:57 -0500

    variables

---
+++ clase-1.py
@@ -0,0 +1,7 @@
+print("Hola mundo")
+print(12+5)
+print(12-5)
+print(12*5)
+print(12/5)
+
+# Con los # puedo realizar un comentario9
---
+++ variables.py
@@ -0,0 +1,23 @@
+# VARAIABLES
+# es un espacio reservado en la memoria
+
+# Variables declarada y asignada un valor
+my_name = "Pedro"
+my_age = 12
+
+# Imprime el contenido de la variable
+print(my_name)
+print(my_age)
+
+# Asignamos un nuevo valor a la variable my_name
+my_name ="Enrique"
+
+#imprime el nuevo valor de la variable my_name
+print(my_name)
+print("Aqui cambio mi nombre por ",my_name)
+
+# LA FUNCION INPUT
+my_name=input("¿Cual es tu numbre?: ")
+my_age=input("¿Cual es tu edad?: ")
+print("Tu nombre es",my_name)
+print("Tu edad es",my_age)
