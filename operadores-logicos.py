bc908b2085f4ee1e8050222c55fb5ee1cb53c51e
pedro-damian <136197537+pedro-damian@users.noreply.github.com>
2023-10-19 16:57:02 -0500

    operadores logicos

---
+++ operadores-logicos.py
@@ -0,0 +1,21 @@
+
+# OPERADOR and
+print("True and True =>", True and True)
+print("True and False =>", True and False)
+print("False and True =>", False and True)
+print("False and False =>", False and False) 
+
+print("10>5 and 5<10 => el valor logico es:",10>5 and 5<10)
+print("10>5 and 5>10 => el valor logico es:",10>5 and 5>10)
+
+stock = int(input("Ingrese un numero de entre 100 y 200: "))
+print("Si el valor ingresado esta entre 100 y 200 mostrara True de lo contrario False => ",stock>=100 and stock<=200)
+
+# OPERADOR or
+print("True or True =>", True or True)
+print("True or False =>", True or False)
+print("False or True =>", False or True)
+print("False or False =>", False or False) 
+
+role = input("Escribe el cargo de admin o supervisor para ingresar: ")
+print("Si el cargo es admin o supervisor dara True de lo contrario false => ", role=="admin" or role=="supervisor")
