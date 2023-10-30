eb85a12885d408fe17f919be265074acf9e9cab2
pedro-damian <136197537+pedro-damian@users.noreply.github.com>
2023-10-30 17:08:27 -0500

    while

--- diccionario.py
+++ diccionario.py
@@ -13,13 +13,17 @@
     "clave3": "valor3"
 } """
 
+# ITEMS (cada par clave-valor) - KEYS (es la clave) - VALUES (es el valor)
+
 mi_diccionario = {
   "nombre": "pedro",
   "apellido": "damian",
   "telefono": 987654321,
-  "edad": 32
+  "edad": 32,
+  "idiomas": ['español','ingles','portugues']
 }
 
+print("--------------------------------------------------------------------")
 print(f"Los elementos de mi diccionario son {mi_diccionario} y es de tipo {type(mi_diccionario)}", )
 
 # metodo len()
@@ -34,3 +38,41 @@ print(f"La clave unica direccion se encuentra en el diccionario => {mi_diccionar
 # tambien puedes verificar si el elemento se encuentra en el diccionario con boolean
 print(f"El diccionario tiene la clave unica direccion => {'direccion' in mi_diccionario}")
 print(f"El diccionario tiene la clave unica nombre => {'nombre' in mi_diccionario}")
+print("--------------------------------------------------------------------")
+
+# ------------------------------ CRUD ------------------------------------
+# ----------------------------- CREATE ------------------------------------
+aula = {
+  "Alumnos": ['pedro','miguel','juan','jose'],
+  "Profesor": "Jose martin",
+  "materias": ['matematicas','lenguaje','ciencias'],
+  "N_alumnos": 30,
+  "grado": "primaria"
+}
+
+# ----------------------------- READ ------------------------------------
+
+print(f"Los elementos del diccionario son => {aula}")
+print(f"Los elementos par clave:valor del diccionario son {aula.items()}")
+print(f"Los elementos clave del diccionario son {aula.keys()}")
+print(f"Los elementos valor del diccionario son {aula.values()}")
+
+# ----------------------------- UPDATE ------------------------------------
+# modificando un valor a uno nuevo
+aula['Profesor'] = 'Manuel perez'
+# operando un valor
+aula['N_alumnos'] -= 10
+# agregando un valor nuevo
+aula['materias'].append('calculo')
+print("--------------------------------------------------------------------")
+print(f"Llave unica actualizada se modifico el valor de profesor a {aula}")
+print(f"Llave unica actualizada se realizo una operacion a N_alumnos {aula}")
+print(f"Llave unica actualizada se agrego una nueva materia {aula}")
+print("--------------------------------------------------------------------")
+# ----------------------------- DELETE ------------------------------------
+# elimina una llave unica del diccionario
+del aula['grado']
+# tambien elimina una llave unica del diccionario
+aula.pop('N_alumnos')
+
+print(f"Llaves unicas eliminadas grado y ")
---
+++ while.py
@@ -0,0 +1,8 @@
+
+# WHILE (mientras la condicion sea true..)
+#  se va ejecutar hasta que sea false
+
+counter = 0
+while counter < 10:
+  counter +=1
+  print(counter)
