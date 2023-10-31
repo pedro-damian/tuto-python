ed8a4c77f5bc7e4572d690a23298bd66b5405c80
pedro-damian <p.damianoriega@gmail.com>
2023-10-30 23:43:56 -0500

    while-2

--- while.py
+++ while.py
@@ -2,7 +2,16 @@
 # WHILE (mientras la condicion sea true..)
 #  se va ejecutar hasta que sea false
 
+# inicializamos una variable en 0
 counter = 0
-while counter < 10:
+# mientras counter es menor que 10
+while counter < 20:
+  # entonces que counter vaya aumentando en uno en uno
   counter +=1
-  print(counter)
+
+  # si counter es igual a 15 
+  if counter == 15:
+    # entonces se detiene
+    break
+
+  print(counter, end=" ")
