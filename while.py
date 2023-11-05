cb8cdd90dc0a1e10abb1d93f2cb46a4dd65b75d4
pedro-damian <136197537+pedro-damian@users.noreply.github.com>
2023-11-05 17:04:11 -0500

    for

--- while.py
+++ while.py
@@ -3,6 +3,32 @@
 #  se va ejecutar hasta que sea false
 
 counter = 0
-while counter < 10:
+
+# mientras el contador sea menor a 10
+#while counter < 10:
+  # contara uno a uno
+  #counter +=1
+  # imprimira del uno al diez
+  #print(counter)
+  
+  
+# BREAK (detiene un ciclo de manera forzada)
+#while counter < 20:
+  #counter +=1
+  # si contador es igual a 10
+  #if counter ==10:
+    # se detiene el proceso
+    #break
+  #print(counter)
+  
+  
+# CONTINUE (salta procesos sin imprimir)
+while counter < 20:
   counter +=1
+  # si contador es mayor a 10
+  if counter > 10:
+    # continue al siguiente proceso sin imprimir
+    continue
+  # imprimira los numeros de numeros 1 a 10
   print(counter)
+  
