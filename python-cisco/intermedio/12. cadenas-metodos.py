15c1550e691899aaf7293ec00d137b864479ca62
pedro <p.damianoriega@gmail.com>
2024-01-21 22:56:16 -0500

    metodos-cisco

--- python-cisco/intermedio/12. cadenas-metodos.py
+++ python-cisco/intermedio/12. cadenas-metodos.py
@@ -22,3 +22,50 @@ print('[' + 'Beta'.center(6) + ']') # [ Beta ]
 
 # La variante de dos parámetros de center() hace uso del carácter del segundo argumento, en lugar de un espacio.
 print('[' + 'gamma'.center(20, '*') + ']') # [*******gamma********]
+
+
+# -------------------- endswith() --------------------------
+# comprueba si la cadena dada termina con el argumento especificado
+# devuelve True(verdadero) o False(falso), dependiendo del resultado.
+
+t = "zeta"
+print(t.endswith("a")) # True
+print(t.endswith("A")) # False
+print(t.endswith("et")) # Flase
+print(t.endswith("eta")) # True
+
+
+# -------------------- find() --------------------------
+# busca una subcadena y devuelve el índice de la primera letra de esta subcadena
+# en caso la subcadena no exista (devuelve -1)
+# Funciona solo con cadenas - no aplica a secuencias (listas,tuplas. etc..)
+
+t = 'theta'
+print(t.find('eta')) # 2
+print(t.find('et')) # 2
+print(t.find('the')) # 0
+print(t.find('ha')) # -1
+
+# find() con 2 parametros, 1 parametro es la subcadena, 2 parametro es de que indice iniciara la busqueda
+
+the_text = """A variation of the ordinary lorem ipsum
+text has been used in typesetting since the 1960s 
+or earlier, when it was popularized by advertisements 
+for Letraset transfer sheets. It was introduced to 
+the Information Age in the mid-1980s by the Aldus Corporation, 
+which employed it in graphics and word-processing templates
+for its desktop publishing program PageMaker (from Wikipedia)"""
+
+fnd = the_text.find('the')
+while fnd != -1:
+    print("La subcadena 'the' se encuentra en el indice:",fnd) # 15, 80, 198, 221, 238
+    fnd = the_text.find('the', fnd + 1) # indica que comienza desde la posición siguiente a la última ocurrencia encontrada es por eso muy importante el +1 de lo contrario entra en un bucle infinito
+
+
+# find() con 3 parametros, 1 subcadena, 2 indice inicio, 3 indice final donde termina la busqueda 
+print('kappa'.find('a', 1, 4)) # 1
+print('kappa'.find('a', 2, 4)) # -1
+
+
+
+
