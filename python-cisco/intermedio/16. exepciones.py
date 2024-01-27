398a54e228280fbfb5afc76b601f4457c8e16c14
pedro <p.damianoriega@gmail.com>
2024-01-26 22:27:21 -0500

    cisco-exepciones

---
+++ python-cisco/intermedio/16. exepciones.py
@@ -0,0 +1,19 @@
+
+# Cada vez que tu código intenta hacer algo erróneo, irresponsable o inaplicable, Python hace dos cosas:
+# Detiene tu programa.
+# Crea un tipo especial de dato, llamado excepción.
+
+#-------------------------------- ZeroDivisionError -------------------------------
+
+value = 1
+value /= 0      # ZeroDivisionError: division by zero
+
+
+#-------------------------------- IndexError ------------------------------
+
+my_list = []
+x = my_list[0]  # IndexError: list index out of range
+
+
+#-------------------------------- IndexError ------------------------------
+
